d5731c9ae33df4b58e1f485c3d2e98a7e0309742
Robert Andersson <h23roban@du.se>
2023-12-12 12:06:22 +0100

    First Push

---
+++ GIK299_L4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System;
+
+namespace GIK299_L3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alex";
+            Gender gender = Gender.ickeBinär;
+            string eyeColor = "Blå";
+            Hair hair = new Hair { Haircolor = "Blond", HairLength = "Kort" };
+            DateTime birthdate = new DateTime(2009, 2, 25);
+            Person person = new Person(name, gender, eyeColor, hair, birthdate);
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine();
+        }
+    }
+}
