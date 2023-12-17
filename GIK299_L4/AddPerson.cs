5759df456b883857ba373982fd9cfd20761e6102
Robert Andersson <h23roban@du.se>
2023-12-17 15:16:53 +0100

    Uppdatering innan ominstallation av datorn

--- GIK299_L4/AddPerson.cs
+++ GIK299_L4/AddPerson.cs
@@ -1,24 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using GIK299_L4;
+using System.Collections.Generic;
 
-//namespace GIK299_L4
-//{
-//    internal class AddPerson
-//    {
-//        private static void AddPerson()
-//        {
-//            string name = "Alex";
-//            Gender gender = Gender.ickeBinär;
-//            string eyeColor = "Blå";
-//            Hair hair = new Hair { Haircolor = "Blond", HairLength = "Kort" };
-//            DateTime birthdate = new DateTime(2009, 2, 25);
-//            Person person = new Person(name, gender, eyeColor, hair, birthdate);
+namespace gik299_l4
+{
+    public class AddPerson
+    {
+        private List<Person> people = new List<Person>();
 
-//            Console.WriteLine(person.ToString());
-//            Console.WriteLine();
-//        }
-//    }
-//}
+        public List<Person> Persons
+        {
+            get { return people; }
+        }
+
+        public void AddPersonToList(Person person)
+        {
+            people.Add(person);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            foreach (var person in people)
+            {
+                result += $"Namn:\t \t {person.Name}\n" +
+                          $"Kön:\t \t {person.Gender}\n" +
+                          $"Ögonfärg:\t {person.EyeColor}\n" +
+                          $"Hår:\t \t {person.Hair.Haircolor},{person.Hair.HairLength}\n" +
+                          $"Födelsedatum:\t {person.BirthDate}\n\n";
+            }
+
+            return result;
+        }
+    }
+}
--- GIK299_L4/Program.cs
+++ GIK299_L4/Program.cs
@@ -2,280 +2,298 @@
 using System;
 using System.Linq.Expressions;
 using System.Security.Cryptography.X509Certificates;
+using gik299_l4;
 
 namespace GIK299_L4
 {
-	public class Program
-	{
-		static void Main(string[] args)
-		{
-			try
-			{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddPerson addPerson = null;
 
-				int menuSelector = 0;
-				int lastMenuOption = 3;
-				bool loopMenu = true;
-
-				while (loopMenu) //en whileloop där variabeln loopMenu är villkoret som avgör om loopen ska fortsätta eller ej
-				{
-					Console.WriteLine(
-						"------------------------------------------------" +
-						"\nVälj ett av alternativen: " +
-						"\n1.) Registrera uppgifter om ny person" +
-						"\n2.) Lista redan registrerade personer" +
-						"\n3.) Avsluta programmet" +
-						"\n------------------------------------------------"
-						);
+            try
+            {
+                int menuSelector = 0;
+                int lastMenuOption = 3;
+                bool loopMenu = true;
 
 
-					Console.Write("> ");
-					bool parseSuccess;
-
-                https://stackoverflow.com/questions/15294878/how-the-int-tryparse-actually-works
-
-                        parseSuccess = int.TryParse(Console.ReadLine(), out menuSelector);
-
-					if (parseSuccess == false) {
-						Console.WriteLine($"Menyvalet måste vara ett heltal mellan 1 och {lastMenuOption}");
-					}
-
-					if (menuSelector == 0 || menuSelector < 0 || menuSelector > lastMenuOption)
-					{
-						Console.WriteLine($"Felaktigt menyval ange ett heltal mellan 1 och {lastMenuOption} ");
-					}
-					else
-					{
-						
-					}
-
-					switch (menuSelector)
-					{
-						case 1:
-							{
-
-								Console.WriteLine("1");
-
-							}
-							break;
-
-						case 2:
-							{
-
-								Console.WriteLine("2");
-
-							}
-							break;
-
-						case 3:
-							{
-								loopMenu = false;
-								Console.WriteLine("Avslutar programmet");
-							}
-							break;
+                while (loopMenu) //en whileloop där variabeln loopMenu är villkoret som avgör om loopen ska fortsätta eller ej
+                {
+                    Console.WriteLine(
+                        "------------------------------------------------" +
+                        "\nVälj ett av alternativen: " +
+                        "\n1.) Registrera uppgifter om ny person" +
+                        "\n2.) Lista redan registrerade personer" +
+                        "\n3.) Avsluta programmet" +
+                        "\n------------------------------------------------"
+                        );
 
 
+                    Console.Write("> ");
+                    bool parseSuccess;
+
+                    
+
+                    parseSuccess = int.TryParse(Console.ReadLine(), out menuSelector);
+                    //("How the int.TryParse actually works," 2013)
+                    // Kontrollerar om det går att parsea strängen, att selectorn inte är 0, innehåller negativa tal
+                    // Eller är större än antalet val på menyn.
+                    if (parseSuccess == false || menuSelector == 0 || menuSelector < 0 || menuSelector > lastMenuOption)
+                    {
+                        Console.WriteLine($"Menyvalet måste vara ett heltal mellan 1 och {lastMenuOption}");
+                    }
+
+                    
+                    switch (menuSelector) //Stora switchen för Huvudmenyn
+                    {
+                        case 1: //Case 1 som skapar upp personobjekt.
+                            {
+
+                                string name = null;
+                                Gender gender = Gender.okänt;
+                                string eyeColor = null; ;
+                                string birthdate = null;
+                                string tempHairLength = null;
+                                string tempHairColor = null;
+                                int genderChoice = 0;
 
 
+                                try // Försöksblocket för personnamn.
+                                {
+                                    bool loop = true;
+                                    while (loop)
+                                    {
+                                        Console.Write("Ange namn på aktuell person: ");
+                                        name = Console.ReadLine();
+
+                                        //Kontrollerar inmatad sträng för att utesluta att strängen är tom.
+                                        if (string.IsNullOrWhiteSpace(name))
+                                        {
+                                            Console.WriteLine("Inget namn angivet, vänligen ange ett korrekt namn. ");
+                                        }
+                                        else
+                                        {
+                                            loop = false;
+                                        }
+
+                                    }
+                                }
+
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Error: {ex.Message}");
+                                }
 
 
+                                try //Tryblock för val av kön.
+                                {   //("Total number of items defined in an enum," 2009
+                                    // Skapar en array med alla kön angivna i enum Gender, vi tar längden på denna array för att veta hur många val
+                                    // som ska finnas i menyn där man väljer kön.
 
-							string name = null;
-							Gender gender = Gender.okänt;
-							string eyeColor = null; ;
-							string birthdate = null;
-							string tempHairLength = null;
-							string tempHairColor = null;
-							int genderChoice = 0;
-
-
-							try
-							{
-								bool loop = true;
-								while (loop)
-								{
-									Console.Write("Ange namn på aktuell person: ");
-									name = Console.ReadLine();
-
-									//Kontrollerar inmatad sträng för att utesluta att strängen är tom.
-									if (string.IsNullOrWhiteSpace(name))
-									{
-										Console.WriteLine("Inget namn angivet, vänligen ange ett korrekt namn. ");
-									}
-									else
-									{
-										loop = false;
-									}
-
-								}
-							}
-
-							catch (Exception ex)
-							{
-								Console.WriteLine($"Error: {ex.Message}");
-							}
+                                    int genderlength = Enum.GetValues(typeof(Gender)).Length;
+                                    bool loop = true;
+                                    while (loop)
+                                    {
 
 
-							try
-							{
-								// https://stackoverflow.com/questions/856154/total-number-of-items-defined-in-an-enum
-								// Skapar en array med alla kön angivna i enum Gender, vi tar längden på denna array för att veta hur många val
-								// som ska finnas i menyn där man väljer kön.
+                                        Console.WriteLine("Välj könstillhörighet\n");
+                                        Console.WriteLine(
+                                            "1. Kvinna \n" +
+                                            "2. Man\n" +
+                                            "3. Icke Binär\n" +
+                                            "4. Annat\n" +
+                                            "5. Okänt"
+                                        );
 
-								int genderlength = Enum.GetValues(typeof(Gender)).Length;
-								bool loop = true;
-								while (loop)
-								{
+                                        bool genderParseSuccess;
+                                        genderParseSuccess = int.TryParse(Console.ReadLine(), out genderChoice);
 
+                                        if (genderParseSuccess == false || genderChoice == 0 || genderChoice < 0 || genderChoice > 5)
+                                        {
+                                            Console.WriteLine($"Felaktigt menyval ange ett heltal mellan 1 och {genderlength}");
+                                        }
+                                        else
+                                        {
+                                            loop = false;
+                                        }
+                                    }
 
-									Console.WriteLine("Välj könstillhörighet\n");
-									Console.WriteLine(
-										"1. Kvinna \n" +
-										"2. Man\n" +
-										"3. Icke Binär\n" +
-										"4. Annat\n" +
-										"5. Okänt"
-									);
+                                    // Use switch statement based on user's choice
 
-									// Read the user's input
-									if (!int.TryParse(Console.ReadLine(), out genderChoice))
-									{
-										Console.WriteLine($"Felaktigt menyval ange ett heltal mellan 1 och {genderlength}");
-									}
-									if (genderChoice == 0 || genderChoice < 0 || genderChoice > 5)
-									{
-										Console.WriteLine($"Felaktigt menyval ange ett heltal mellan 1 och {genderlength}");
-									}
-									else
-									{
-										loop = false;
-									}
-								}
+                                    switch (genderChoice)
+                                    {
+                                        case 1:
+                                            gender = Gender.kvinna;
+                                            break;
 
-								// Use switch statement based on user's choice
+                                        case 2:
+                                            gender = Gender.man;
+                                            break;
 
-								switch (genderChoice)
-								{
-									case 1:
-										gender = Gender.kvinna;
-										break;
+                                        case 3:
+                                            gender = Gender.ickeBinar;
+                                            break;
 
-									case 2:
-										gender = Gender.man;
-										break;
+                                        case 4:
+                                            gender = Gender.annat;
+                                            break;
 
-									case 3:
-										gender = Gender.ickeBinar;
-										break;
+                                        case 5:
+                                            gender = Gender.okänt;
+                                            break;
 
-									case 4:
-										gender = Gender.annat;
-										break;
+                                        default:
+                                            Console.WriteLine("Ogiltigt val");
+                                            return;
+                                    }
 
-									case 5:
-										gender = Gender.okänt;
-										break;
+                                    Console.WriteLine($"Du har valt kön: {gender}");
 
-									default:
-										Console.WriteLine("Ogiltigt val");
-										return;
-								}
+                                }
+                                catch (Exception)
+                                {
 
-								Console.WriteLine($"Du har valt kön: {gender}");
+                                    throw;
+                                }
+                                //Kontrollerar inmatningen av ögonfärg, om inget värde anges värdet till okänt.
+                                try
+                                {
+                                    Console.Write("Ange ögonfärg (Om okänt tryck endast enter):");
+                                    string tempEyeColor = Console.ReadLine();
 
-							}
-							catch (Exception)
-							{
+                                    if (string.IsNullOrWhiteSpace(tempEyeColor))
+                                    {
+                                        eyeColor = "Okänt";
+                                    }
+                                    else
+                                    {
+                                        eyeColor = tempEyeColor;
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Ett oväntat fel uppstod");
+                                }
 
-								throw;
-							}
-							//Kontrollerar inmatningen av ögonfärg, om inget värde anges värdet till okänt.
-							try
-							{
-								Console.Write("Ange ögonfärg (Om okänt tryck endast enter):");
-								string tempEyeColor = Console.ReadLine();
+                                try
+                                {
+                                    Console.Write("Ange hårlängd (Om okänt eller inget hår tryck enter) ");
+                                    tempHairLength = Console.ReadLine();
 
-								if (string.IsNullOrWhiteSpace(tempEyeColor))
-								{
-									eyeColor = "Okänt";
-								}
-								else
-								{
-									eyeColor = tempEyeColor;
-								}
-							}
-							catch (Exception ex)
-							{
-								Console.WriteLine("Ett oväntat fel uppstod");
-							}
-
-							try
-							{
-								Console.Write("Ange hårlängd (Om okänt eller inget hår tryck enter) ");
-								tempHairLength = Console.ReadLine();
-
-								if (string.IsNullOrWhiteSpace(tempHairLength))
-								{
-									tempHairLength = "Okänt/Ej Hår";
-								}
+                                    if (string.IsNullOrWhiteSpace(tempHairLength))
+                                    {
+                                        tempHairLength = "Okänt/Ej Hår";
+                                    }
 
 
-								Console.Write("Ange hårfärg (Tryck om okänt eller inget hår tryck enter) ");
-								tempHairColor = Console.ReadLine();
-								if (string.IsNullOrWhiteSpace(tempHairColor))
-								{
-									tempHairColor = "Okänt / Ej Hår";
-								}
+                                    Console.Write("Ange hårfärg (Tryck om okänt eller inget hår tryck enter) ");
+                                    tempHairColor = Console.ReadLine();
+                                    if (string.IsNullOrWhiteSpace(tempHairColor))
+                                    {
+                                        tempHairColor = "Okänt / Ej Hår";
+                                    }
 
-							}
-							catch (Exception ex)
-							{
-								Console.WriteLine("Ett oväntat fel uppstod");
-							}
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Ett oväntat fel uppstod");
+                                }
 
-							try
-							{
-								Console.Write("Ange födelsedatum (Om ej känt tryck enter): ");
-								string tempBirtdate = Console.ReadLine();
-								if (string.IsNullOrWhiteSpace(tempBirtdate))
-								{
-									birthdate = "Okänt / Ej känt";
-								}
-								else
-								{
-									birthdate = tempBirtdate;
-								}
-							}
-							catch (Exception)
-							{
+                                try
+                                {
+                                    Console.Write("Ange födelsedatum (Om ej känt tryck enter): ");
+                                    string tempBirtdate = Console.ReadLine();
+                                    if (string.IsNullOrWhiteSpace(tempBirtdate))
+                                    {
+                                        birthdate = "Okänt";
+                                    }
+                                    else
+                                    {
+                                        birthdate = tempBirtdate;
+                                    }
+                                }
+                                catch (Exception)
+                                {
 
-								throw;
-							}
+                                    throw;
+                                }
 
-							//Lagt initieringen av struct för hår utanför alla try catch för att den inte ville gå vidare till constructorn.
-							//Felsöks i mån av tid.
+                                //Lagt initieringen av struct för hår utanför alla try catch för att den inte ville gå vidare till constructorn.
+                                //Felsöks i mån av tid.
 
-							Hair hair = new Hair { HairLength = tempHairLength, Haircolor = tempHairColor };
+                                Hair hair = new Hair { HairLength = tempHairLength, Haircolor = tempHairColor };
+
+                                Person person = new Person(name, gender, eyeColor, hair, birthdate);
+                                if (addPerson == null)
+                                {
+                                    addPerson = new AddPerson();
+                                }
+                                addPerson.AddPersonToList(person);
+
+                                break;
+                            }
+
+                    
+                
+
+
+                            //Person person = new Person(name, gender, eyeColor, hair, birthdate);
+                            //AddPerson addperson = new AddPerson(person);
+
+                    //personList.Add(person);
+
+                    //Console.WriteLine(person.ToString());
+                    //Console.WriteLine();
+
+                    //RestaurantTable table3 = new RestaurantTable(3, 2, Reservation.NotReserved);
+                    //table3.SeatGuests(false);
+                    //restaurantTables.Add(table3);
 
 
 
 
-							Person person = new Person(name, gender, eyeColor, hair, birthdate);
 
-							Console.WriteLine(person.ToString());
-							Console.WriteLine();
+                        case 2:
+                            {
+                                if (addPerson == null || addPerson.Persons.Count == 0)
+                                {
+                                    Console.WriteLine("Listan över personer är tom, börja med att lägga till personer först.");
+                                }
+                                else
+                                {
+                                    // Visa lagrade personer
+                                    Console.WriteLine(addPerson.ToString());
+                                }
+
+                                break;
+                            }
+
+                        case 3:
+                            {
+                                loopMenu = false;
+                                Console.WriteLine("Avslutar programmet");
+
+                            }
+                            break;
+                    }
+                }
+            }
+
+            catch (Exception ex)//Catch för huvudmenyn
+            {
+                Console.WriteLine(ex.Message);
 
 
-					}
-				}
-			}
-			catch (Exception ex)//Catch för huvudmenyn
-			{
-				Console.WriteLine(ex.Message);
-				
+            }
 
-			}
+        }
+    }
 
-		}
-	}
+    /* Referenser:
+     * https://stackoverflow.com/questions/15294878/how-the-int-tryparse-actually-works */
+    // https://stackoverflow.com/questions/856154/total-number-of-items-defined-in-an-enum
 }
+
 
